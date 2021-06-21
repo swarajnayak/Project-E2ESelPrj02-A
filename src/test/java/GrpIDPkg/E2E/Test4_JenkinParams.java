3d023ab882a18fee9bffbc714329bb1c694760d9
swaraj <swarajnayak015@gmail.com>
2021-06-22 00:38:13 +0530

    from eclipse project A 12:37AM

--- src/test/java/GrpIDPkg/E2E/Test4_JenkinParams.java
+++ src/test/java/GrpIDPkg/E2E/Test4_JenkinParams.java
@@ -15,6 +15,7 @@ public class Test4_JenkinParams extends Init
     	String browserName = System.getProperty("browser");
     	System.out.println(browserName);
     	System.out.println("First clone from GITHUB 11:58 PM");
+    	System.out.println("Second change from eclipse prj02-A 12:36 AM");
       
     }
     
