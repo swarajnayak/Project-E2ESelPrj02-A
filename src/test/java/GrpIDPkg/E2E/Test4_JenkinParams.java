33469a3c073f084859f861ddd839c4fa49c6d7de
swaraj <swarajnayak015@gmail.com>
2021-06-23 20:21:47 +0530

    Change from master branch - E2ESelPrj02-B 8:18 PM

--- src/test/java/GrpIDPkg/E2E/Test4_JenkinParams.java
+++ src/test/java/GrpIDPkg/E2E/Test4_JenkinParams.java
@@ -17,6 +17,7 @@ public class Test4_JenkinParams extends Init
     	System.out.println("First clone from GITHUB 11:58 PM");
     	System.out.println("Second change from eclipse prj02-A 12:36 AM");
     	System.out.println("Change from develp branch - E2ESelPrj02-A 7:39 PM");
+    	System.out.println("Change from master branch - E2ESelPrj02-B 8:18 PM");
       
     }
     
